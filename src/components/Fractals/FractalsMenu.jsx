0967dfb37bf1eef76fc0472d16a1ea8b92004e12
Анна-Марія <anna-mariia.kurhanevych.pz.2021@lpnu.ua>
2023-10-16 13:28:16 +0300

    Changed save as button to save

--- src/components/Fractals/FractalsMenu.jsx
+++ src/components/Fractals/FractalsMenu.jsx
@@ -79,7 +79,7 @@ function FractalsMenu({
         }}
         onClick={hadleImageSaving}
       >
-        Save as ...
+        Save
       </Button>
       <Button
         onClick={handleFractalSelect}
