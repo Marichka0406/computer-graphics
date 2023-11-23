582beea24d67e94aa9838fee835e959303aed0b0
Yehor Lesnevych <yehor.lesnevych.pz.2021@lpnu.ua>
2023-11-23 02:49:10 +0200

    Changed algorithm (matrix)

--- src/components/MovingImages/MovingTrapezoid.jsx
+++ src/components/MovingImages/MovingTrapezoid.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
 
+const numeric = require('numeric');
+
 const MovingTrapezoid = () => {
     // Визначення координат трапеції
     const [points, setPoints] = useState({
-        x: [1, 2, 4, 3, 1],
-        y: [1, 3, 4, 2, 1],
+        x: [1, 2, 3, 6, 1],
+        y: [1, 3, 3, 1, 1],
         name: ['Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 1']
     });
 
@@ -30,16 +32,82 @@ const MovingTrapezoid = () => {
         setPoints(updatedPoints);
     };
 
-    const rotatePoint = (x, y, x0, y0, rotateDegree) => {
-        let radian = rotateDegree * Math.PI / 180.0;
-        let cos = Math.round(Math.cos(radian) * 1000) / 1000;
-        let sin = Math.round(Math.sin(radian) * 1000) / 1000;
+    // Функція для створення матриці трансформації для масштабування та повороту
+    function getTransformationMatrix(centerX, centerY, scale, angleInDegrees) {
+        // Перетворення кута в радіани
+        const angleInRadians = angleInDegrees * (Math.PI / 180);
 
-        let newX = (x - x0) * cos - (y - y0) * sin + x0;
-        let newY = (x - x0) * sin + (y - y0) * cos + y0;
+        let cos = Math.round(Math.cos(-angleInRadians) * 1000) / 1000;
+        let sin = Math.round(Math.sin(-angleInRadians) * 1000) / 1000;
 
-        return { x: newX, y: newY };
-    };
+        // Матриця трансформації для здійснення повороту відносно точки
+        const rotationMatrix = [
+            [cos, -sin, 0],
+            [sin, cos, 0],
+            [0, 0, 1],
+        ];
+
+        const scalingMatrix = [
+            [1/scale, 0, 0],
+            [0, 1/scale, 0],
+            [0, 0, 1],
+          ];
+
+        // Матриця трансформації для зміщення точки в центрі обертання
+        const translationMatrixToCenter  = [
+            [1, 0, -centerX],
+            [0, 1, -centerY],
+            [0, 0, 1],
+        ];
+
+        // Матриця трансформації для повернення точки назад на місце
+        const translationMatrixFromCenter = [
+            [1, 0, centerX],
+            [0, 1, centerY],
+            [0, 0, 1],
+        ];
+
+        // Порядок застосування матриць: зміщення в центр, масштабування, поворот, зміщення назад
+        const transformationMatrix = multiplyMatrices(
+            translationMatrixToCenter,
+            scalingMatrix,
+            rotationMatrix,
+            translationMatrixFromCenter,
+        );
+
+        return transformationMatrix;
+    }
+
+    function getTransformedPoint(matrix, point) {
+        // Додайте третю координату (однорідні координати)
+        const homogeneousPoint = [point.x, point.y, 1];
+
+        // Обернена матриця
+        const inverseMatrix = numeric.inv(matrix); // numeric - це бібліотека для операцій з матрицями (потрібно встановити через npm)
+
+        // Помножте вектор на обернену матрицю
+        const transformedPoint = numeric.dot(inverseMatrix, homogeneousPoint);
+
+        // Поверніть координати точки
+        return { x: transformedPoint[0], y: transformedPoint[1] };
+    }
+
+    // Функція для множення матриць
+    function multiplyMatrices(...matrices) {
+        return matrices.reduce(
+            (result, matrix) =>
+                result.map((row, i) =>
+                    row.map(
+                        (_, j) => matrix[i][0] * result[0][j] + matrix[i][1] * result[1][j] + matrix[i][2] * result[2][j]
+                    )
+                ),
+            [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+            ]
+        );
+    }
 
     const handleRotateDegreeChange = (value) => {
         setRotateDegree(value);
@@ -51,48 +119,16 @@ const MovingTrapezoid = () => {
     };
 
     const handleTransformOnClick = () => {
-        // Scale
-        for (let i = 0; i < points.x.length; ++i) {
-            points.x[i] = points.x[i] * scale;
-        }
-
-        for (let i = 0; i < points.y.length; ++i) {
-            points.y[i] = points.y[i] * scale;
-        }
-
-        const scaledPoints = {
-            x: [...points.x],
-            y: [...points.y],
-            name: [...points.name]
-        };
-
-        setPoints(scaledPoints);
-
         // Point for rotation
         const x0 = points.x[rotationPoint];
         const y0 = points.y[rotationPoint];
 
-        // Rotate
-        let newPoints = [
-            { x: points.x[0], y: points.y[0] },
-            { x: points.x[1], y: points.y[1] },
-            { x: points.x[2], y: points.y[2] },
-            { x: points.x[3], y: points.y[3] },
-            { x: points.x[4], y: points.y[4] },
-        ];
+        let transformedPoint;
 
-        newPoints.forEach(point => {
-            let newPoint = rotatePoint(point.x, point.y, x0, y0, rotateDegree);
-            point.x = newPoint.x;
-            point.y = newPoint.y;
-        });
-
-        for (let i = 0; i < points.x.length; ++i) {
-            points.x[i] = newPoints[i].x;
-        }
-
-        for (let i = 0; i < points.y.length; ++i) {
-            points.y[i] = newPoints[i].y;
+        for (let i = 0; i < points.x.length && i < points.y.length; ++i) {
+            transformedPoint = getTransformedPoint(getTransformationMatrix(x0, y0, scale, rotateDegree), { x: points.x[i], y: points.y[i] },);
+            points.x[i] = transformedPoint.x;
+            points.y[i] = transformedPoint.y;
         }
 
         const updatedPoints = {
@@ -102,14 +138,14 @@ const MovingTrapezoid = () => {
         };
 
         setPoints(updatedPoints);
-    };
+    }
 
     const handleRotatePointChange = (val) => {
         setRotationPoint(val);
     };
 
     return (
-        <div>
+        <div style={{ marginTop: 100 }}>
             <Plot
                 data={[
                     {
